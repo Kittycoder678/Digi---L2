28dba45ab2c4be39dda4fe1095476b04eaad3d35
felicitywright <felicitywright2410@gmail.com>
2025-07-29 09:50:56 +1200

    Added in first main page worth of code and finished V1 of my code

--- javascript.js
+++ javascript.js
@@ -5,6 +5,15 @@ function openMenu() {
     menuBox.style.display = 'none';
     menuTitles.style.display = 'none';
 }
+
+function openMen() {
+    menuBars.style.display = 'initial';
+    menuTest.style.display = 'initial';
+    menuTitle.style.display = 'initial';
+    menuBox.style.display = 'none';
+    menuTitles.style.display = 'none';
+}
+
 function reset() {
     menuBox.style.display = 'initial';
     menuBar.style.display = 'none';
@@ -13,5 +22,13 @@ function reset() {
     menuTitle.style.display = 'none';
 }
 
-menuBox.onmouseover = openMenu;
-menuBar.onmouseleave = reset;
+function rese() {
+    menuBox.style.display = 'initial';
+    menuBars.style.display = 'none';
+    menuTitles.style.display = 'initial';
+    menuTest.style.display = 'none';
+    menuTitle.style.display = 'none';
+}
+
+menuBox.onmouseover = openMenu & openMen;
+menuBar.onmouseleave = reset & rese;
